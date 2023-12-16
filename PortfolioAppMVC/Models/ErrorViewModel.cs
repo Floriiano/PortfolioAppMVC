f7981ed27416f72f39193e711f3eaa99915493a9
Tiago Floriano de Lima <tiago_damg@hotmail.com>
2023-12-16 18:08:23 -0300

    Add project files.

---
+++ PortfolioAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortfolioAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
